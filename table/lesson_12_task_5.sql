408ada970e44f03550d456ec5fef610d347d44b7
Badizawr <vicodin-13@ya.ru>
2025-02-19 14:17:55 +0500

    update leson13 task6

--- table/lesson_12_task_5.sql
+++ table/lesson_12_task_5.sql
@@ -11,3 +11,8 @@ ADD COLUMN IF NOT EXISTS direction VARCHAR(100);
 
 ALTER TABLE students 
 ADD COLUMN IF NOT EXISTS direction_id INTEGER REFERENCES directions(id);
+
+
+SELECT s.id, s.first_name, s.last_name, s.exam_score, d.name AS direction_name
+FROM students s
+JOIN directions d ON s.direction_id = d.id;
